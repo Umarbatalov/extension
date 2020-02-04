7cbb6399232b7f6133c06f25d596abbee59860d6
umarbatalov <umarbatalov@mail.ru>
2020-02-04 19:14:31 +0300

    Init

---
+++ background.js
@@ -0,0 +1,60 @@
+const iframe = document.getElementsByTagName('iframe')[0],
+  textArea = iframe.contentWindow.document.body,
+  after = document.getElementsByClassName('style_container__1qOGd')[0];
+
+let NewElement;
+
+function insertAfter (newElement, targetElement) {
+  let parent = targetElement.parentNode;
+
+  if (parent.lastChild === targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement, targetElement.nextSibling);
+  }
+}
+
+textArea.addEventListener('input', () => {
+  let char = textArea.innerText.slice(-1);
+
+  if (char === ':') {
+    NewElement = document.createElement('div');
+
+    NewElement.setAttribute(
+      'style',
+      'position: absolute;' +
+      'bottom: 48px;' +
+      'height: 61px;' +
+      'width: 354px;' +
+      'background-color: lightgrey;' +
+      'border-radius: 21px;' +
+      'text-align: center;' +
+      'padding-top: 16px;' +
+      'display: flex;'
+    );
+
+    for (let i = 0; i <= 3; i++) {
+      let child = document.createElement('div');
+
+      child.setAttribute(
+        'style',
+        'width: 69px;' +
+        'background-color: white;' +
+        'height: 35px;' +
+        'margin-left: 15px;' +
+        'border-radius: 21px;' +
+        'padding-top: 11px;'
+      );
+
+      child.innerText = '(ू˃̣̣̣̣̣̣︿˂̣̣̣̣̣̣ ू)';
+
+      NewElement.appendChild(child);
+    }
+
+    insertAfter(NewElement, after);
+  } else {
+    if (typeof NewElement !== 'undefined') {
+      NewElement.remove();
+    }
+  }
+});
